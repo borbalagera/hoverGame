246c3439b167a7bdaaf44d9a7bf75e7dfb67ce81
Gera Borbála <37974097+borbalagera@users.noreply.github.com>
2020-03-05 14:52:10 +0100

    added a counter

--- hoverGame.js
+++ hoverGame.js
@@ -1,11 +1,25 @@
+var clicks =0;
+
+function counterUp(){
+    clicks++;
+    document.getElementById("counter").innerHTML = clicks;
+}
+
+function reset(){
+clicks=0;
+clicks--;
+document.getElementById("counter").innerHTML = clicks;
+}
+
 function caught(){
     alert("omg you did it");
 }
 var w = window.innerWidth-100;
 var h = window.innerHeight-100;
+
 $(function(){
     $("button").on({
-        mouseover:function(){
+        mouseenter:function(){
             $(this).css({
                 left:(Math.random()*w)+"px",
                 top:(Math.random()*h)+"px",
