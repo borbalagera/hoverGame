e42e114bf36031e01d2f4439ab431d9421fe69a5
borbalagera <borbalagera@gmail.com>
2020-07-10 12:38:49 +0200

    Styling updated.

--- hoverGame.js
+++ hoverGame.js
@@ -1,15 +1,29 @@
-function caught(){
-    alert("omg you did it");
+var clicks = 0;
+
+function counterUp() {
+  clicks++;
+  document.getElementById("counter").innerHTML = clicks;
 }
-var w = window.innerWidth-100;
-var h = window.innerHeight-100;
-$(function(){
-    $("button").on({
-        mouseover:function(){
-            $(this).css({
-                left:(Math.random()*w)+"px",
-                top:(Math.random()*h)+"px",
-            });
-        }
-    });
+
+function reset() {
+  clicks = 0;
+  clicks--;
+  document.getElementById("counter").innerHTML = clicks;
+}
+
+function caught() {
+  alert("omg you did it");
+}
+var w = window.innerWidth - 100;
+var h = window.innerHeight - 100;
+
+$(function () {
+  $("button").on({
+    mouseenter: function () {
+      $(this).css({
+        left: Math.random() * w + "px",
+        top: Math.random() * h + "px",
+      });
+    },
+  });
 });
